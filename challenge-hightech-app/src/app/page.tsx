23d6040da5e393256780bc80a893c3db833569c0
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-03-25 17:41:38 -0300

    Starting Project

---
+++ challenge-hightech-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <>
+      <h1>Landing page</h1>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </>
+  );
+}
