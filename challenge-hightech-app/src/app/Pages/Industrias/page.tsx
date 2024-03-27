78dc4fc8bcc2ac851be03d24bf2bb8947fd761b2
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-03-26 21:34:05 -0300

    Industrias Card / Page

--- challenge-hightech-app/src/app/Pages/Industrias/page.tsx
+++ challenge-hightech-app/src/app/Pages/Industrias/page.tsx
@@ -1,12 +1,25 @@
 'use client'
+import Card from "@/app/Components/Card_Industrias/page";
 import Image from "next/image";
+import "./Industrias.css"
 
 
 export default function Industrias() {
   return (
     <>
-      <h1>Industrias</h1>
-  
+      <section className="Industrias_title">
+        <div className="Industrias_text">
+          <h1>Torne-se uma empresa cliente líder em seu setor.</h1>
+          <p>Nossas soluções são criadas por especialistas do setor para ajuidá-lo a modernizar seus negócios , economizar tempo, reduzir custos e aproximar-se de seus clientes.</p>
+        </div>
+        <Image src={"/assets/Industrias.svg"} alt="" width={100} height={100}/>
+      </section>
+      
+      <h1 className="sub_Industrias_title">Escolha sua indústria para começar :</h1>
+     
+      <section className="cardGrid">
+        <Card/>
+      </section>
     </>
   );
 }
