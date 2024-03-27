c0c49ccd81fc12686209958ef52381e7c76294c9
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-03-26 23:56:18 -0300

    Empresa / Suporte

--- challenge-hightech-app/src/app/Components/Footer/page.tsx
+++ challenge-hightech-app/src/app/Components/Footer/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Image from "next/image";
 import "./Footer.css"
+import Link from "next/link";
 
 
 export default function Footer() {
@@ -10,9 +11,9 @@ export default function Footer() {
         <div className="footer_content">
             <Image className="footer_content_img" src="/assets/Logos.svg" width={100} height={100} alt=""/>
             <ul className="footer_list">
-                <li><a href="" target="_blank">Dev Teams</a></li>
-                <li><a href="" target="_blank">Organograma</a></li>
-                <li><a href="" target="_blank">GitHub</a></li>
+                <li><Link href="" target="_blank">Dev Teams</Link></li>
+                <li><Link href="" target="_blank">Organograma</Link></li>
+                <li><Link href="" target="_blank">GitHub</Link></li>
             </ul>
         </div>
         <div className="footer_copyright"> &#169; 2023 all rights reserved.</div>
--- challenge-hightech-app/src/app/Pages/Suporte/page.tsx
+++ challenge-hightech-app/src/app/Pages/Suporte/page.tsx
@@ -1,10 +1,19 @@
 'use client'
 import Image from "next/image";
+import Card from "@/app/Components/Card_Suporte/page";
+import "./Suporte.css"
 
 export default function Suporte() {
   return (
     <>
-      <h1>Suporte</h1>
+      <div className="Suporte_title">
+        <h1>Dúvidas? Conte com a gente.</h1>
+        <p>Encontre as respostas que você mais precisa para tudo da Salesforce, procure recursos, explore os documentos, registre casos e muito mais.</p>
+      </div>
+
+      <section className="cardGrid">
+        <Card/>
+      </section>
     </>
   );
 }
