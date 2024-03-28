ff4d3947351230bf81bad054e78962512a6953b0
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-03-27 22:03:35 -0300

    Cadastro

--- challenge-hightech-app/src/app/Pages/Cadastro/page.tsx
+++ challenge-hightech-app/src/app/Pages/Cadastro/page.tsx
@@ -1,16 +1,57 @@
 'use client'
 import Image from "next/image";
+import { useForm, SubmitHandler } from "react-hook-form"
 import Link from "next/link";
+import "./Cadastro.css";
 
 export default function Cadastro() {
+  const { register, handleSubmit } = useForm()
+
+  function createUser(date: any) {
+    console.log(date)
+
+  }
+
   return (
     <>
-      <Image src="/assets/salesforce_negativa.svg" width={250} height={250} alt="Logo da Sales Force"/>
-      <h1>Cadastro</h1>
+      <section className="section_Cadastro">
+        <Image src="/assets/salesforce_negativa.svg" width={250} height={250} alt="Logo da Sales Force"/>
+        <h1>Cadastro</h1>
+
+        <form onSubmit={handleSubmit(createUser)} className="form_Cadastro">
+          <div className="input-box">
+            <label>E-mail: </label>
+            <input type="email" {...register('email')} placeholder="E-mail do usuário"/>
+          </div>
+
+          <div className="input-box">
+            <label>Nome: </label>
+            <input type="text" {...register('nome')} placeholder="Nome do usuário"/>
+          </div>
+
+          <div className="input-box">
+            <label>Cargo: </label>
+            <input type="text" {...register('cargo')} placeholder="Cargo do usuário"/>
+          </div>
+
+          <div className="input-box">
+            <label>Telefone: </label>
+            <input type="tel" {...register('tel')} placeholder="Telefone do usuário"/>
+            <small>Format: (99)99999-9999</small>
+          </div>
+
+          <div className="input-box">
+            <label>Senha: </label>
+            <input type="password" {...register('password')} placeholder="Senha"/>
+          </div>
+        </form>
+
+        <button className="login">
+          <Link href={"/"}>Finalizar Cadastro</Link>
+        </button> 
+      </section>
       
-      <button className="login">
-        <Link href={"/"}>Finalizar Cadastro</Link>
-      </button> 
+      
     </>
   );
 }
