8b8c550e4cbc6330a40b7ce289bf89f08bbe912c
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-03-30 23:37:36 -0300

    card devs

--- challenge-hightech-app/src/app/Pages/DevTeams/page.tsx
+++ challenge-hightech-app/src/app/Pages/DevTeams/page.tsx
@@ -11,17 +11,19 @@ export default function DevTeams() {
 
       <section className="cardGrid">
         <div className="card">
-          <Image  src={""} alt={""} width={150} height={150}/>
+          <Image className="imgUser" src={"/assets/Luccas.svg"} alt={"Imagem do Luccas"} width={150} height={150}/>
           <h1>Luccas Silva</h1>
           <h2>RM: 552890</h2>
         </div>
+
         <div className="card">
-          <Image  src={""} alt={""} width={150} height={150}/>
+          <Image className="imgUser" src={"/assets/Bruno.svg"} alt={"Imagem do Bruno"} width={150} height={150}/>
           <h1>Bruno Burian</h1>
           <h2>RM: 552863</h2>
         </div>
+        
         <div className="card">
-          <Image  src={""} alt={""} width={150} height={150}/>
+          <Image className="imgUser" src={"/assets/Nathalia.svg"} alt={"Imagem do Nathalia"} width={150} height={150}/>
           <h1>Nathalia Freire</h1>
           <h2>RM: 553233</h2>
         </div>
