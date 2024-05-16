b8661eb4d2b841818bef345ddd3fa424a4994d38
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-05-15 21:40:06 -0300

    creating new java project

---
+++ Challenge_HighTech_Java/Salesforce/src/main/java/com/challenge/app/App.java
@@ -0,0 +1,16 @@
+package com.challenge.app;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ola mundo");
+		
+		sc.close();
+
+	}
+
+}
--- Challenge_HighTech_Java/src/bd/Credenciais.java
+++
@@ -1,6 +0,0 @@
-package bd;
-
-public class Credenciais {
-	public static final String user = "rm552890"; 
-	public static final String pwd = "130604";
-}
